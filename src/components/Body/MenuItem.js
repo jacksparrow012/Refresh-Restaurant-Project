22f5f908e7891bdb8bfbd0cac25d467fe43efc8b
Nazrul islam <jacksparrow.alex@gmail.com>
2021-03-03 15:12:13 +0600

    menuItem component now showing all dish

--- src/components/Body/MenuItem.js
+++ src/components/Body/MenuItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { Card, CardBody, CardImg, CardTitle } from "reactstrap"
 const MenuItem = (props) => {
     console.log(props.dish)
     return (
         <div>
+            <Card style={{ marginTop: "15px", border: "1px solid grey", boxShadow: "2px 2px #888888", textAlign: "center" }}>
+                <CardBody>
+                    <CardTitle>{props.dish.name}</CardTitle>
+                    <CardImg src={props.dish.image} alt={props.dish.name} />
 
+                </CardBody>
+            </Card>
         </div>
     );
 };
