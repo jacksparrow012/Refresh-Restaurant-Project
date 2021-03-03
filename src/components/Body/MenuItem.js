17760cdf5cc8de524e41a957b3de2c4a8454c1fa
Nazrul islam <jacksparrow.alex@gmail.com>
2021-03-03 15:16:13 +0600

    some style added to title of our dishes

--- src/components/Body/MenuItem.js
+++ src/components/Body/MenuItem.js
@@ -6,7 +6,14 @@ const MenuItem = (props) => {
         <div>
             <Card style={{ marginTop: "15px", border: "1px solid grey", boxShadow: "2px 2px #888888", textAlign: "center" }}>
                 <CardBody>
-                    <CardTitle>{props.dish.name}</CardTitle>
+                    <CardTitle style={{
+                        fontSize: "22px",
+                        fontWeight: "bold",
+                        backgroundColor: "#D70F62",
+                        borderRadius: "5px",
+                        color: "white",
+                        fontFamily: "Arial",
+                    }}>{props.dish.name}</CardTitle>
                     <CardImg src={props.dish.image} alt={props.dish.name} />
 
                 </CardBody>
