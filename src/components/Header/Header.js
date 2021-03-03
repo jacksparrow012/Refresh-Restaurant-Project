a159fc53a838a7bd9744461901eae9d85fc4a8ff
Nazrul islam <jacksparrow.alex@gmail.com>
2021-03-03 14:07:24 +0600

    Navigation bar created with navbarBrand

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Navigation from "./Navigation"
 const Header = () => {
     return (
         <div>
-            <h3>Header</h3>
+            <Navigation />
         </div>
     );
 };
