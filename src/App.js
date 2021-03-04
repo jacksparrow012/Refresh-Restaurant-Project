a3661fe9d554a86643f2e4da85878a47f68c6ae7
Nazrul islam <jacksparrow.alex@gmail.com>
2021-03-04 11:44:47 +0600

    reducer function and store created so now redux store is availavle for everywhere in our project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import MainComponent from './components/MainComponent'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux"
+import myStore from './redux/store'
 function App() {
   return (
     <div className="App">
+      <Provider store={myStore}>
+        <BrowserRouter>
+          <MainComponent />
 
-      <BrowserRouter>
-        <MainComponent />
-
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
